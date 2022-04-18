befdc99b78b81b4fa3102980157a76b5f856faa2
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-04-18 17:29:54 -0400

    wip

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -35,7 +35,16 @@ const checkSchemeId = async (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
+  const { scheme_name } = req.body
+  if (
+    scheme_name === undefined || 
+    typeof scheme_name !== 'string' ||
+    !scheme_name.trim()
+  ) {
+    next({ status: 400, message: 'invalid scheme name' })
+  } else {
+    next()
+  }
 }
 
 /*
